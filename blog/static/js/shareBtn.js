b2c004fed1859fd7015e4bca9966e6083a5c518d
Vahe4836 <88284245+Vahe4836@users.noreply.github.com>
2024-05-09 14:50:46 +0400

    option  for fixing bug

--- blog/static/js/shareBtn.js
+++ blog/static/js/shareBtn.js
@@ -1,11 +1,15 @@
+const post_detail_slider_buttons_div = document.getElementById("post_detail_slider_buttons_div");
+
 const openShareDropdown = (id) => {
 
     const sharedProject = document.getElementById(id);
     const selectedCompStyle = sharedProject.getElementsByClassName("share_dropdown_div");
     if (selectedCompStyle[0].classList.contains("active")) {
         selectedCompStyle[0].classList.remove("active");
+        post_detail_slider_buttons_div.style.display = "flex";
     } else {
         selectedCompStyle[0].classList.add("active");
+        post_detail_slider_buttons_div.style.display = "none";
     }
 };
 
