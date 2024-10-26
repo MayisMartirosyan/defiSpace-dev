84e2357802ad6fa3d6607c62fc9b677dee4b99b8
Mayis <mayis11.98@gmail.com>
2024-10-26 13:49:31 +0400

    last change before legend task

--- blog/static/js/companies/buildRedirectUrl.js
+++ blog/static/js/companies/buildRedirectUrl.js
@@ -7,7 +7,7 @@ const handle_build_redirect_url = (id) =>{
         url += `&tag_rating=${element.id}`
     }
 
-    url += "&page=1"
+    url += "&page=1#timeline_tags_article"
     
     window.location.href = url
 
