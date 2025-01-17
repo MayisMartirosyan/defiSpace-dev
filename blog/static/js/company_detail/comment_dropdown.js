8323ddfae067d3cbc85f8bfac10cb7aac769e8de
Vahe4836 <88284245+Vahe4836@users.noreply.github.com>
2025-01-17 23:57:39 +0400

    make full responsive company detail page

--- blog/static/js/company_detail/comment_dropdown.js
+++ blog/static/js/company_detail/comment_dropdown.js
@@ -1,14 +1,14 @@
-const add_comment_asset_secured_p = document.getElementById("cmp_det_add_comment_asset_secured_p");
+// let add_comment_asset_secured_p = document.getElementById("cmp_det_add_comment_asset_secured_p");
 // const span = document.getElementById("cmp_det_sub_criterion_2").getElementsByTagName("span")[1].textContent;
 // let current_element = document.getElementById("cmp_det_sub_criterion_2");
 
 // console.log("span", span);
 
 
-function ForTest(event){
-  console.log("event", event);
-  add_comment_asset_secured_p.innerText = event.target;
-}
+// function ForTest(event){
+//   console.log("event", event);
+//   // add_comment_asset_secured_p.innerText = event.target;
+// }
 
 
 // current_element.addEventListener("click", () => {
