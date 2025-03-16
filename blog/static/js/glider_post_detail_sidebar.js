9ac805ab9b2babe6c20e050ea3dde1feb64af862
Vahe4836 <88284245+Vahe4836@users.noreply.github.com>
2025-03-16 23:15:00 +0400

    fix dropdown and delete home element from footer

--- blog/static/js/glider_post_detail_sidebar.js
+++ blog/static/js/glider_post_detail_sidebar.js
@@ -1,10 +1,10 @@
-new Glider(document.querySelector('.glider-post-detail-sidebar-slider'), {
-    // Mobile-first defaults
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    scrollLock: true,
-    arrows: {
-      prev: '.glider-prev-post-detail-sidebar',
-      next: '.glider-next-post-detail-sidebar'
-    },
-  });
+new Glider(document.querySelector(".glider-post-detail-sidebar-slider"), {
+  // Mobile-first defaults
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  scrollLock: true,
+  arrows: {
+    prev: ".glider-prev-post-detail-sidebar",
+    next: ".glider-next-post-detail-sidebar",
+  },
+});
