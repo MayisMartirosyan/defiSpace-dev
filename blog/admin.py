90c96e819c0740d32f5298ad0e4f21b6c5e76856
Mayis <mayis11.98@gmail.com>
2025-03-22 20:14:12 +0400

    asd

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Post, Company, Review, Guides, About, SecurityScore, TeamScore, ProductScore, Advantage, TagPosts, TagRating
+from django import forms
+from django.core.exceptions import ValidationError
+from .models import (
+    Post, Company, Review, Guides, About, SecurityScore, TeamScore,
+    ProductScore, Advantage, TagPosts, TagRating
+)
 
 class TagRatingInline(admin.TabularInline):
     model = Post.tag_rating.through
@@ -9,25 +14,97 @@ class PostAdmin(admin.ModelAdmin):
     inlines = [
         TagRatingInline,
     ]
-    exclude = ('tag_rating',)  # исключаем поле tag_rating из формы создания и редактирования
+    exclude = ('tag_rating',)
+
+# Custom form to enforce validation
+class ScoreForm(forms.ModelForm):
+    def clean_asset_secured_score(self):
+        value = self.cleaned_data.get('asset_secured_score', 1)  # Default value if empty
+        if value < 1:
+            raise ValidationError("Value must be at least 1 and no more than 100.")
+        elif value > 100:
+            raise ValidationError("Value must be at least 1 and no more than 100.")
+        return value
+
+    def clean_emission_limit_score(self):
+        value = self.cleaned_data.get('emission_limit_score', 1)  # Default value if empty
+        if value < 1:
+            raise ValidationError("Value must be at least 1 and no more than 100.")
+        elif value > 100:
+            raise ValidationError("Value must be at least 1 and no more than 100.")
+        return value
+
+    def clean_liquidity_score(self):
+        value = self.cleaned_data.get('liquidity_score', 1)  # Default value if empty
+        if value < 1:
+            raise ValidationError("Value must be at least 1 and no more than 100.")
+        elif value > 100:
+            raise ValidationError("Value must be at least 1 and no more than 100.")
+        return value
+
+    def clean_decentralized_score(self):
+        value = self.cleaned_data.get('decentralized_score', 1)  # Default value if empty
+        if value < 1:
+            raise ValidationError("Value must be at least 1 and no more than 100.")
+        elif value > 100:
+            raise ValidationError("Value must be at least 1 and no more than 100.")
+        return value
+
+    def clean_performace_score(self):
+        value = self.cleaned_data.get('performace_score', 1)  # Default value if empty
+        if value < 1:
+            raise ValidationError("Value must be at least 1 and no more than 100.")
+        elif value > 100:
+            raise ValidationError("Value must be at least 1 and no more than 100.")
+        return value
+
+    def clean_apy_1yr_score(self):
+        value = self.cleaned_data.get('apy_1yr_score', 1)  # Default value if empty
+        if value < 1:
+            raise ValidationError("Value must be at least 1 and no more than 100.")
+        elif value > 100:
+            raise ValidationError("Value must be at least 1 and no more than 100.")
+        return value
+
+    def clean_apy_5yr_score(self):
+        value = self.cleaned_data.get('apy_5yr_score', 1)  # Default value if empty
+        if value < 1:
+            raise ValidationError("Value must be at least 1 and no more than 100.")
+        elif value > 100:
+            raise ValidationError("Value must be at least 1 and no more than 100.")
+        return value
+
+    class Meta:
+        widgets = {
+            'asset_secured_score': forms.NumberInput(attrs={'min': 1, 'max': 100}),
+            'emission_limit_score': forms.NumberInput(attrs={'min': 1, 'max': 100}),
+            'liquidity_score': forms.NumberInput(attrs={'min': 1, 'max': 100}),
+            'decentralized_score': forms.NumberInput(attrs={'min': 1, 'max': 100}),
+            'performace_score': forms.NumberInput(attrs={'min': 1, 'max': 100}),
+            'apy_1yr_score': forms.NumberInput(attrs={'min': 1, 'max': 100}),
+            'apy_5yr_score': forms.NumberInput(attrs={'min': 1, 'max': 100}),
+        }
 
 class SecurityScoreInline(admin.StackedInline):
     model = SecurityScore
     extra = 1
     max_num = 1
     fields = ['asset_secured_score', 'emission_limit_score', 'liquidity_score']
+    form = ScoreForm
 
 class TeamcoreInline(admin.StackedInline):
     model = TeamScore
     extra = 1
     max_num = 1
     fields = ['decentralized_score', 'performace_score']
+    form = ScoreForm
 
 class ProductScoreInline(admin.StackedInline):
     model = ProductScore
     extra = 1
     max_num = 1
     fields = ['performace_score', 'apy_1yr_score', 'apy_5yr_score']
+    form = ScoreForm
 
 class AdvantageInline(admin.TabularInline):
     model = Advantage
@@ -35,17 +112,9 @@ class AdvantageInline(admin.TabularInline):
 
 class AboutAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
-     
-        if About.objects.exists():
-            return False  
-        else:
-            return True  
-        
-        
-
+        return not About.objects.exists()
 
 @admin.register(Company)
-
 class CompanyAdmin(admin.ModelAdmin):
     inlines = [SecurityScoreInline, TeamcoreInline, ProductScoreInline, AdvantageInline]
 
--- blog/models.py
+++ blog/models.py
@@ -2,22 +2,20 @@ from django.db import models
 
 class TagPosts(models.Model):
     name = models.CharField(max_length=50)
-    def str(self):
+    def __str__(self):
         return self.name
 
 class TagRating(models.Model):
     name = models.CharField(max_length=50, null=True, blank=True)
-    def str(self):
+    def __str__(self):
         return self.name
 
 class Guides(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    def str(self):
+    def __str__(self):
         return self.title
 
-
-    
 class Company(models.Model):
     name = models.CharField(max_length=200)
     ticker = models.CharField(max_length=200)
@@ -31,16 +29,15 @@ class Company(models.Model):
     mark = models.BooleanField('Отображать на главной в Projects Scoring', default=False, null=True)
     related_posts = models.ManyToManyField('Post', related_name='related_companies', blank=True)
 
-
     def __str__(self):
         return self.name
 
 class ProductScore(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='product_scores')
-    performace_score = models.IntegerField(default=0)
-    apy_1yr_score = models.IntegerField(default=0)
-    apy_5yr_score = models.IntegerField(default=0)
-    total_score = models.IntegerField(default=0)
+    performace_score = models.IntegerField(default=1)
+    apy_1yr_score = models.IntegerField(default=1)
+    apy_5yr_score = models.IntegerField(default=1)
+    total_score = models.IntegerField(default=1)
 
     def save(self, *args, **kwargs):
         self.total_score = (self.performace_score + self.apy_1yr_score + self.apy_5yr_score) / 3
@@ -48,9 +45,9 @@ class ProductScore(models.Model):
 
 class TeamScore(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='team_scores')
-    decentralized_score = models.IntegerField(default=0)
-    performace_score = models.IntegerField(default=0)
-    total_score = models.IntegerField(default=0)
+    decentralized_score = models.IntegerField(default=1)
+    performace_score = models.IntegerField(default=1)
+    total_score = models.IntegerField(default=1)
 
     def save(self, *args, **kwargs):
         self.total_score = (self.decentralized_score + self.performace_score) / 2
@@ -58,15 +55,14 @@ class TeamScore(models.Model):
 
 class SecurityScore(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='security_scores')
-    asset_secured_score = models.IntegerField(default=0)
-    emission_limit_score = models.IntegerField(default=0)
-    liquidity_score = models.IntegerField(default=0)
-    total_score = models.IntegerField(default=0)
+    asset_secured_score = models.IntegerField(default=1)
+    emission_limit_score = models.IntegerField(default=1)
+    liquidity_score = models.IntegerField(default=1)
+    total_score = models.IntegerField(default=1)
 
     def save(self, *args, **kwargs):
         self.total_score = (self.asset_secured_score + self.emission_limit_score + self.liquidity_score) / 3
         super().save(*args, **kwargs)
-
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -79,33 +75,26 @@ class Post(models.Model):
     pin = models.BooleanField('Отображать на главной в Editor Pick', default=False, null=True)
     company = models.ForeignKey(Company, on_delete=models.SET_NULL, related_name='post', null=True)
 
-    def str(self):
+    def __str__(self):
         return self.title
-    
-    
+
 class About(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    def str(self):
+    def __str__(self):
         return self.title
-    
 
-# Category model for better structure (optional, if you want to categorize comments)
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
 
-# User model for comment authors (if you don't have a user model, replace with a simple string)
 class Author(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
-
-# Comment model to represent both main comments and replies
-from django.db import models
 
 class Comment(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='comments', null=True, blank=True) 
@@ -115,17 +104,14 @@ class Comment(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
-    # Likes and Dislikes
     likes = models.PositiveIntegerField(default=0)
     dislikes = models.PositiveIntegerField(default=0)
 
-    # Reply functionality (Self-referencing ForeignKey)
     parent = models.ForeignKey(
         'self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies'
     )
     
     objects = models.Manager()  
-
 
     def __str__(self):
         return f"Comment by {self.name} in {self.category.name if self.category else 'No Category'}"
@@ -149,7 +135,6 @@ class Comment(models.Model):
         """Returns the number of dislikes"""
         return self.dislikes
 
-# Optionally, a model to track which users liked/disliked
 class CommentReaction(models.Model):
     comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
@@ -158,16 +143,17 @@ class CommentReaction(models.Model):
     def __str__(self):
         return f"{'Like' if self.like else 'Dislike'} by {self.user.username}"
 
-
 class Advantage(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='advantages')
     position = models.PositiveIntegerField()
     advantage = models.CharField(max_length=255)
     mark = models.BooleanField()
     count = models.IntegerField()
+
     class Meta:
         ordering = ['position']
-    def str(self):
+
+    def __str__(self):
         return self.advantage
 
 class Position(models.Model):
@@ -175,7 +161,8 @@ class Position(models.Model):
     email = models.EmailField()
     rating = models.BooleanField()
     comment = models.TextField()
-    def str(self):
+
+    def __str__(self):
         return self.name
     
 class Review(models.Model):
@@ -184,5 +171,6 @@ class Review(models.Model):
     rating = models.BooleanField()
     comment = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    def str(self):
+
+    def __str__(self):
         return self.name
