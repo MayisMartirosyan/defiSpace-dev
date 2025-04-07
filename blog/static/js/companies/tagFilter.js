ee3eaaf4d930ceb42c7f8c2df6f1f4851683b6b8
MayMartirosyan <mayis11.98@gmail.com>
2025-04-08 01:02:35 +0400

    asd

--- blog/static/js/companies/tagFilter.js
+++ blog/static/js/companies/tagFilter.js
@@ -1,3 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const filter_btn_cmp = document.getElementById(`tags_selector_div_mobile`);
+  const filter_btn_cmp_desk = document.getElementById(`tags_selector_div_desktop`);
+  const filter_fixed_popup_div_cmp = document.getElementById("comp_filter_fixed_popup_div");
+  const timeline_tags_div_cmp = document.getElementById("cmp_timeline_tags_div");
+
+  filter_btn_cmp?.addEventListener("click", () => {
+    filter_fixed_popup_div_cmp.style.display = "flex";
+  });
+
+  filter_btn_cmp_desk?.addEventListener("click", () => {
+    filter_fixed_popup_div_cmp.style.display = "flex";
+  });
+
+  filter_fixed_popup_div_cmp?.addEventListener("click", () => {
+    filter_fixed_popup_div_cmp.style.display = "none";
+  });
+
+  timeline_tags_div_cmp?.addEventListener("click", (e) => {
+    e.stopPropagation();
+  });
+});
+
+
 const loadTagFilterInputs = () => {
   const filterInputs = getUrlParams();
 
@@ -22,7 +46,12 @@ const loadTagFilterInputs = () => {
   }
 };
 
+
+
 document.addEventListener("DOMContentLoaded", loadTagFilterInputs);
+
+
+
 
 const handleFilterSubmit = () => {
   let url = "";
